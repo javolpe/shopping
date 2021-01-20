d651574fc01691e8523580f269916cd561bc7886
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-20 09:23:20 -0700

    Add more robust testing for stock,check_stock,inventory testing

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -23,8 +23,22 @@ class VendorTest < Minitest::Test
   def test_stock_method
     vendor = Vendor.new("Rocky Mountain Fresh")
     item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
     vendor.stock(item1, 30)
     expected = {item1 => 30}
+
+    assert_equal expected, vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
+
+    vendor.stock(item1, 25)
+    expected = {item1 => 55}
+
+    assert_equal expected, vendor.inventory
+
+    vendor.stock(item2, 12)
+
+    expected = {item1 => 55,
+                item2 => 12}
     
     assert_equal expected, vendor.inventory
   end
