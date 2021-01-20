89a2352551a8f89508685f49073ff1016677f28e
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-20 09:27:59 -0700

    Create market class, pass init test

--- test/market_test.rb
+++ test/market_test.rb
@@ -3,13 +3,14 @@ require 'minitest/pride'
 require './lib/item'
 require './lib/vendor'
 require './lib/market'
+require 'pry'
 
 class MarketTest < Minitest::Test 
   def test_it_exists_and_has_attributes
     market = Market.new("South Pearl Street Farmers Market")
 
     assert_instance_of Market, market 
-    assert_equal "South Pearl Street Farmers Market and shop", market.name 
+    assert_equal "South Pearl Street Farmers Market", market.name 
     assert_equal [], market.vendors
   end
 end
